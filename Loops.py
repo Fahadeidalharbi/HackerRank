e5f975c59f22be130243b63f2cde75d239aa2632
Fahad Al-Harbi <58704854+Fahadeidalharbi@users.noreply.github.com>
2021-08-14 16:01:14 +0300

    Organize the repo

--- Loops.py
+++
@@ -1,8 +0,0 @@
-if __name__ == '__main__':
-    n = int(raw_input())
-    for i in range(n):
-        print(i**2)
-    
-    #another Solution
-    #print(*[number**2 for number in range(n)], sep = "\n")
-
